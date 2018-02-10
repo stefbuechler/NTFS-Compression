2452cd4953d223e1bc8ca4121d56c5391c18e211
stefbuechler <36002004+stefbuechler@users.noreply.github.com>
2018-02-10 13:51:42 +0100

    Create
    Create V1.0

---
+++ EnableNtfsCompression.ps1
@@ -0,0 +1,21 @@
+﻿#Enables NTFS Compression on diffrent Paths
+#Hint: Run as schduled Task. Otherwiese new files will not get compressed
+#V1.0 Create Skript Stefan Buchler
+
+#Variables
+$CurrentPath = 0
+
+#Paths
+$Path1 = "D:\Data\Backup"
+$Path2 = "D:\Data\Dokumente\BBFN"
+$Path3 = "D:\Data\Dokumente\Buchhaltung"
+$Path4 = "D:\Data\Dokumente\Freizeit"
+$Path5 = "D:\Data\Dokumente\Knowledge"
+$Path6 = "D:\Data\Dokumente\Schule"
+
+Get-ChildItem $Path1 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
+Get-ChildItem $Path2 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
+Get-ChildItem $Path3 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
+Get-ChildItem $Path4 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
+Get-ChildItem $Path5 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
+Get-ChildItem $Path6 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
