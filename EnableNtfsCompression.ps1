93024fc6ab5eaf67ec736738d3c60510031f4ff3
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-02-11 12:13:27 +0100

    Removed duplicate code

--- EnableNtfsCompression.ps1
+++ EnableNtfsCompression.ps1
@@ -2,17 +2,12 @@
 # Hint: Run as schduled Task. Otherwise new files will not get compressed
 # V1.0 Create Skript Stefan Buechler 10.02.2018
 
-# Paths
-$Path1 = "D:\Data\Backup"
-$Path2 = "D:\Data\Dokumente\BBFN"
-$Path3 = "D:\Data\Dokumente\Buchhaltung"
-$Path4 = "D:\Data\Dokumente\Freizeit"
-$Path5 = "D:\Data\Dokumente\Knowledge"
-$Path6 = "D:\Data\Dokumente\Schule"
+@(
+    "D:\Data\Backup",
+    "D:\Data\Dokumente\BBFN",
+    "D:\Data\Dokumente\Buchhaltung",
+    "D:\Data\Dokumente\Freizeit",
+    "D:\Data\Dokumente\Knowledge",
+    "D:\Data\Dokumente\Schule"
+) | ForEach-Object { Get-ChildItem $_ -Recurse | Where-Object { $_.Attributes -notlike "*Compressed*" } | ForEach-Object { compact /C $_.FullName } }
 
-Get-ChildItem $Path1 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
-Get-ChildItem $Path2 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
-Get-ChildItem $Path3 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
-Get-ChildItem $Path4 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
-Get-ChildItem $Path5 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
-Get-ChildItem $Path6 -Recurse | Where-Object {$_.Attributes -notlike "*Compressed*"} | ForEach-Object {compact /C $_.FullName}
